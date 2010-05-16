265fcec51f2b06a13e2b92e61a33332c924c4e85
takatoh <takatoh@mbg.nifty.com>
2010-05-16 20:02:32 +0900

    app.rb: Cleaning.

--- app.rb
+++ app.rb
@@ -13,42 +13,38 @@ require 'lazy_album'
 
 class LazyAlbumApp < Sinatra::Base
 
-  helpers do
-    include Rack::Utils
-    alias_method :h, :escape_html
-    include LazyAlbum::Utils
-  end
-
-  include LazyAlbum::HTMLHelper
-
-
   set :run, true
 
   enable :static
   set :public, File.dirname(__FILE__) + "/public"
-  enable :methodoverride
-  enable :sessions
 
+  def initialize
+    @config = LazyAlbum::Config.instance
+  end
 
-  ## Methods
-  def odd_or_even(n)
-    ["even", "odd"][n % 2]
+  # Helpers
+  helpers do
+    include Rack::Utils
+    alias_method :h, :escape_html
+    include LazyAlbum::Utils
+    include LazyAlbum::HTMLHelper
+
+    def odd_or_even(n)
+      ["even", "odd"][n % 2]
+    end
   end
 
 
   # Index
   get '/' do
-    @config = LazyAlbum::Config.instance
     @page_title = @config.page_title
     @ents = LazyAlbum::Entries.new.serch
-    @items = @ents.to_array
-    @items.sort!{|a, b| a[:title] <=> b[:title] }
+    @items = @ents.to_array.sort{|a, b| a[:title] <=> b[:title] }
     erb :index
   end
 
   # Send thumbnail
   get '/images/*.thumbnail' do
-    @config = LazyAlbum::Config.instance
     path = params[:splat][0].split('/')
     entry = conv_to_filesystem_encoding(path[0..-2].join('/'))
     thumbnail = "tn_" + path.last
@@ -57,14 +53,12 @@ class LazyAlbumApp < Sinatra::Base
 
   # Send picture
   get '/images/*' do
-    @config = LazyAlbum::Config.instance
     path = conv_to_filesystem_encoding(params[:splat][0])
     send_file "#{@config.data_dir}/#{path}"
   end
 
   # Picture
   get '/*' do
-    @config = LazyAlbum::Config.instance
     pathes = params[:splat][0].split("/")
     @picture = pathes[-1]
     pass unless LazyAlbum.picture?(@picture)
@@ -74,9 +68,7 @@ class LazyAlbumApp < Sinatra::Base
 
   # Entry
   get '/*' do
-    @config = LazyAlbum::Config.instance
     @entry = params[:splat][0].sub(/\A\//, "")
-
     @ent = LazyAlbum::Entry.new(@entry)
     begin
       @ent.read
@@ -84,12 +76,9 @@ class LazyAlbumApp < Sinatra::Base
     end
     @title = @ent.title || "(no title)"
     @items = @ent.pictures
-
     @ent.search
-    @sub_entries = @ent.sub_entries.to_array
-    @sub_entries.sort!{|a, b| a[:title] <=> b[:title] }
+    @sub_entries = @ent.sub_entries.to_array.sort{|a, b| a[:title] <=> b[:title] }
     @ent.make_thumbnail_all
-
     erb :entry
   end
 
